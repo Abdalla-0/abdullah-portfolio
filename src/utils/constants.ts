e08226f1c7160ad1682164d29f7acef6fb196696
Abdalla-0 <136491297+Abdalla-0@users.noreply.github.com>
2025-02-15 20:49:45 +0200

    fix db file

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export enum Directions {
+    RTL = "rtl",
+    LTR = "ltr",
+}
+
+export enum Languages {
+    ENGLISH = "en",
+    ARABIC = "ar",
+}
+
+export enum Methods {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    PATCH = "PATCH",
+    DELETE = "DELETE",
+}
+
+export enum Environments {
+    PROD = "production",
+    DEV = "development",
+}
