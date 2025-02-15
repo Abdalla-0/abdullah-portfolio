9841277158712ac95399a18eeaac429beb8de5e0
Abdalla-0 <136491297+Abdalla-0@users.noreply.github.com>
2025-02-15 20:31:26 +0200

    fix bugs to deploy

---
+++ prisma/migrations/20250215165615_/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "stack" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Skill" (
+    "id" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Skill_pkey" PRIMARY KEY ("id")
+);
--- src/server/db/skills.ts
+++ src/server/db/skills.ts
@@ -1,28 +1,29 @@
 "use server";
 import { cache } from "@/utils/cache";
 import { db } from "@/utils/db";
+import { revalidateTag } from "next/cache";
 
 export const actionAddSkill = async () => {
-  try {
-    const skill = await db.skill.create({
-      data: {
-        title: "skill-4",
-        image: "/bootstrap.png",
-      },
-    });
 
-    console.log("Skill added:", skill); // ✅ هتطبع في الكونسول
-    return skill;
-  } catch (error) {
-    console.error("Error adding skill:", error);
-  }
+  const skill = await db.skill.create({
+    data: {
+      title: "skill-4",
+      image: "/bootstrap.png",
+    },
+  });
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  revalidateTag("skills");
+  return skill;
+
 }
 
 export const getSkills = cache(
   async () => {
     const skills = await db.skill.findMany();
+
     return skills;
   },
-  ["products"],
-  { revalidate: 3600 }
+  ["skills"],
+  { revalidate: false, tags: ["skills"] }
+
 );
