fbdf3bf2291f63b1d4a3093df9e2c2694a20b0f9
Abdalla-0 <136491297+Abdalla-0@users.noreply.github.com>
2025-09-10 18:22:04 +0300

    edit next.config file

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,12 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = withNextIntl({
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-        port: "",
-        pathname: "/**",
-      },
+      { protocol: "https", pathname: "**", hostname: "res.cloudinary.com" },
+      { protocol: "http", pathname: "**", hostname: "res.cloudinary.com" },
     ],
   },
   experimental: {
